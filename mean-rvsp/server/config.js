8c739e60664b19cdb4923fb2b4ab08794018b522
Rogbet <rogbet@hotmail.com>
2017-12-18 01:55:06 -0600

    Add Node

---
+++ mean-rvsp/server/config.js
@@ -0,0 +1,6 @@
+// server/config.js
+module.exports = {
+    AUTH0_DOMAIN: 'rogbet.auth0.com', // e.g., kmaida.auth0.com
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://roger:roger123@ds161016.mlab.com:61016/mean'
+  };
